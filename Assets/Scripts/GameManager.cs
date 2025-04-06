4494d19c9b0d6b63fab2349cb795710f8d6d2817
Benjamin <bybenh@gmail.com>
2025-04-06 22:50:12 +0200

    Save/Checkpoints

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,14 +6,15 @@ public class GameManager : MonoBehaviour
 {
     public float movementSpeed =7;
     public GameObject levelGroup;
-    void Start()
+    public Vector3 lastCheckpoint = new Vector3(0,0,0);
+    public void ResetPositions()
     {
-        
+        levelGroup.transform.position = new Vector3(lastCheckpoint.x, 0, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateCheckpoint(Vector3 newCheckpoint)
     {
-        
+        lastCheckpoint = newCheckpoint; 
     }
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,6 +25,9 @@ public class PlayerController : MonoBehaviour
                 inAir = true;
             }
         }
+
+        
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
